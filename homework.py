22fc30e571384dcaef8128d9bdb17064ef20edee
Jurgita Gestautas <jnj0205@gmail.com>
2023-05-02 11:19:08 -0500

    first commit

---
+++ homework.py
@@ -0,0 +1,44 @@
+#Exercise #1
+#Cube Number Test... Print out all cubed numbers up to the total value 1000#
+#. Meaning that if the cubed number is over 1000 break the loop.#
+
+list=[1,1000]
+
+for i in range(1,1001):
+    cube = i**3
+    list.append(cube)
+    print(cube)
+if cube == 1000:
+    break
+
+print(list)
+
+
+#Get first prime numbers up to 100#
+prime_num = list(1,100)
+
+for num in prime_num:
+    if num ==2
+    print(num)
+    if num % != 2
+    if num % ==5
+    print(num)
+    if num % 5
+
+
+###Take in a users input for their age, if they are younger
+#  than 18 print kids, if they're 18 to 65 print adults, 
+# else print seniors. Gracefully log the error if the user doesn't enter 
+# a number###
+
+age=int(age)
+    if age is <= 18
+        print(kids)
+    if age is <=65
+        print(adults)
+    else:
+        print(seniors)
+
+print()
+
+
